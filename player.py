7c8587d3cfc0da9caee29ab49d161ee434a845cc
rafael simao <raficher@gmail.com>
2014-05-04 17:06:10 -0700

    Changed player so that the actions were applied in an action function instead of happening into the receive_input method.

--- player.py
+++ player.py
@@ -17,7 +17,7 @@ class Player(GameObject, InputListener):
         self.projectile_list = list()
         self.fire_delay = 15
         
-        self.move_right = self.move_left = self.fire_shot = False
+        self.is_moving_right = self.is_moving_left = self.is_firing = False
     
     'Makes the player actions _move and fire'
     def update(self, dt):
@@ -31,14 +31,14 @@ class Player(GameObject, InputListener):
             
     'Moves the player, based in the current pressed buttons'
     def _move(self, dt):
-        if self.move_left and self.box.left > 0:
+        if self.is_moving_left and self.box.left > 0:
             self.box.left -= self.speed*dt
-        elif self.move_right and self.box.left < 900:
+        elif self.is_moving_right and self.box.left < 900:
             self.box.left += self.speed*dt
      
     'Allows the player to fire projectiles if correct button is pressed'
     def _shoot(self):
-        if self.fire_shot and self.fire_delay == 15:
+        if self.is_firing and self.fire_delay == 15:
             projectile = Projectile(self.box.left+22.5, self.box.top, -0.4)
             self.projectile_list.append(projectile)
             #print(self.projectile_list.__len__())
@@ -68,20 +68,32 @@ class Player(GameObject, InputListener):
         #Starts moving
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
-                self.move_left = True
+                self.move_left(True)
             elif event.key == pygame.K_d:
-                self.move_right = True
+                self.move_right(True)
         #Finishes moving
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_a:
-                self.move_left = False
+                self.move_left(False)
             elif event.key == pygame.K_d:
-                self.move_right = False
+                self.move_right(False)
         #Starts firing projectile
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                self.fire_shot = True
+                self.fire_shot(True)
         #Finishes firing projectile
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_SPACE:
-                self.fire_shot = False
+                self.fire_shot(False)
+    
+    'Move Actions'            
+    def move_left(self, is_moving):
+        self.is_moving_left = is_moving
+        
+    def move_right(self, is_moving):
+        self.is_moving_right = is_moving
+        
+    'Fire Actions'            
+    def fire_shot(self, is_firing):
+        self.is_firing = is_firing
+        
