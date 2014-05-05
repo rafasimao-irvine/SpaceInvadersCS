64ccf4c126035a9912a2a81b4068e8d50b06ac21
rafael simao <raficher@gmail.com>
2014-05-04 17:04:19 -0700

    Created new manager for the server and a new state_game for the server.

---
+++ manager_server.py
@@ -0,0 +1,74 @@
+import pygame
+from input_manager import InputManager
+from state_game_server import StateGameServer
+from server import start_server
+
+#initiate the pygame
+pygame.init()
+fpsClock = pygame.time.Clock()
+#print("initializing clock: ", fpsClock)
+
+#Manager class
+class Manager:
+
+    ##Screen
+    width, height = 950, 600
+    size = width, height
+    
+    screen  = pygame.display.set_mode(size)
+
+    #pygame.display.set_caption("SpaceInvaders")        
+    
+    #InputManager
+    inputManager = InputManager()
+
+    #Introduction state
+    state = StateGameServer(screen, inputManager)
+     
+    #Game started check
+    game_started = False
+    #Main Loop
+    def _run(self):
+        self.gameOn = True
+        
+        while self.gameOn:
+            dt = fpsClock.tick(30)
+
+            #Inputs
+            self.inputManager.update()
+            
+            if self.game_started == True:
+                #Updates
+                self.update(dt)
+                
+            #Renders, put in the screen
+            #self.render()
+        
+    
+    #Update
+    def update(self, dt):
+        #state updates
+        new_state = self.state.update(dt)
+        if (new_state == 0):
+            return
+       
+    def set_state(self, state):
+        self.state.destroy()
+        self.state = state
+    
+    #Render
+    def render(self):
+        #state renders
+        self.state.render()
+        
+        #updates the display
+        pygame.display.update()
+        
+        
+
+#Run the main loop
+if "__main__" == __name__:
+                
+    manager = Manager()
+    start_server()
+    manager._run()
---
+++ state_game_server.py
@@ -0,0 +1,93 @@
+import pygame
+from state import State
+from player import Player
+from invaders_manager import InvadersManager
+#from Invaders import Invaders
+
+'''
+Main game state. Might be the class where the whole game will run at.
+'''
+class StateGameServer(State):
+
+    players_list = list()
+    #invader = Invaders(0)
+    invader_manager = InvadersManager()
+
+    def __init__(self, screen, inputManager):
+        State.__init__(self, screen, inputManager)
+        
+        self.board_bounds = pygame.Rect(0,0,screen.get_width(),screen.get_height())
+        
+        self.fontObj = pygame.font.Font('freesansbold.ttf', 22)
+        
+        
+    def destroy(self): pass
+    
+    
+    '''Update'''
+    def update(self, dt):
+        State.update(self, dt) 
+
+        #Updates the game objects
+        for player in self.players_list:
+            player.update(dt)
+        #self.invader.update(dt)
+        self.invader_manager.update(dt)
+
+        #treats projectiles hits        
+        self._treat_invader_projectiles()
+        self._treat_player_projectiles()
+        
+    'Make invaders projectiles collisions and perform the consequences'
+    def _treat_invader_projectiles(self):
+        if self.invader_manager.projectile_list.__len__() > 0: 
+            #Goes through all the invaders projectiles
+            for shot in self.invader_manager.projectile_list:
+                #If it is out of the board game box, it is removed
+                if not self._remove_if_out_of_board(self.invader_manager.projectile_list, shot):
+                    #If it collides with the player, the player receives the damage and the projectile is removed
+                    if shot.is_colliding_with(self.player):
+                        self.player.receive_hit()
+                        self.invader_manager.projectile_list.remove(shot)
+
+    'Make players projectiles collisions and perform the consequences'
+    def _treat_player_projectiles(self):
+        if self.player.projectile_list.__len__() > 0: 
+            #Goes through all the invaders projectiles
+            for shot in self.player.projectile_list:
+                #If it is out of the board game box, it is removed
+                if not self._remove_if_out_of_board(self.player.projectile_list, shot):
+                    collided = False
+                    for invader in self.invader_manager.invaders_list:
+                        if not collided and shot.is_colliding_with(invader):
+                            self.player.projectile_list.remove(shot)
+                            self.invader_manager.invaders_list.remove(invader)
+                            self.player.increase_score(15)
+                            #self.invader_manager.speedUp()
+                            collided = True
+                        
+                    
+    'Removes a projectile from a projectile list if it is out of the board bounds.' 
+    'Returns True if it is removed and False if it is not.'
+    def _remove_if_out_of_board(self, projectile_list, projectile):
+        #If it is out of the board game box, it is removed
+        if not self.board_bounds.colliderect(projectile.get_collision_box()):
+            projectile_list.remove(projectile)
+            return True
+        
+        return False
+
+    
+    '''Render'''
+    def render(self):
+        State.render(self) 
+        #background
+        self.screen.fill(pygame.Color(0,0,0))
+        
+        #render objects
+        for player in self.players_list:
+            player.render(self.screen)
+        self.invader_manager.render(self.screen)
+        
+        
+        
