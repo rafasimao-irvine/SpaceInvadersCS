16e4e22cc47e97babf84d5dad5743877634e46a6
rafael simao <raficher@gmail.com>
2014-05-04 17:04:47 -0700

    Created client and server to deal with the connection.

--- client.py
+++ client.py
@@ -1,40 +1,30 @@
 from network import Handler, poll
-import sys
+#import sys
 from threading import Thread
 
-
-myname = raw_input('What is your name? ')
+#myname = raw_input('What is your name? ')
 
 class Client(Handler):
     
     global myname
     
     def on_close(self):
-        global running
-        running = False
         print "****** Disconnected from server ******"
     
-    def on_msg(self, msg):
-        if 'txt' in msg and msg['speak'] != myname:
-            print msg['speak'] +": "+ msg['txt']
-        elif 'joined' in msg:
-            print msg['joined']+ " joined. Users: "+msg['users']
-        elif 'left' in msg:
-            print msg['left']+ " left the room. Users: "+msg['users']
-        
-host, port = 'localhost', 8888
-client = Client(host, port)
-client.do_send({'join': myname})
+    def on_msg(self, msg): pass
+    
+    
 
 def periodic_poll():
     while 1:
         poll(0.05)  # seconds
                             
-thread = Thread(target=periodic_poll)
-thread.daemon = True  # die when the main thread dies 
-thread.start()
+def start_client():
+    host, port = 'localhost', 8888
+    #Client(host, port)
+    client = Client(host, port)
+    client.do_send({'join':'JOINED!'})
 
-running = True #running state
-while running:
-    mytxt = sys.stdin.readline().rstrip()
-    client.do_send({'speak': myname, 'txt': mytxt})
+    thread = Thread(target=periodic_poll)
+    thread.daemon = True  # die when the main thread dies 
+    thread.start()
--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from network import Listener, Handler, poll
 
+from threading import Thread
  
 handlers = {}  # map client handler to user name
 
-class MyHandler(Handler):
+class ServerHandler(Handler):
     
     global handlers
     
@@ -14,44 +15,23 @@ class MyHandler(Handler):
         del handlers[self] #remove from the dictionary
      
     def on_msg(self, msg):
-
-        message = "" # message that will be sent back
-    
-        # Joining the room
-        if 'join' in msg:
-            handlers[self] = msg['join'] #put the user in the dictionary
-            # tell clients that someone joined
-            message = {'joined' : msg['join'], 'users' : self._get_all_users()}
-        
-        # Txt msg
-        else:
-            # Leaving the room
-            if msg['txt'] == "quit":
-                self.do_close()
-                # tell the others he left
-                message = {'left':msg['speak'], 'users' : self._get_all_users()}
-
-            else:
-                message = msg # send it back
-    
-        if message != "":
-            self._send_to_all_users(message)
-        #print msg
+        print msg
 
 
     def _send_to_all_users(self,msg):
         for h in handlers:
             h.do_send(msg)
 
-    def _get_all_users(self):
-        users = ""
-        for h in handlers:
-            users = handlers[h] if users == "" else users +","+handlers[h]
-        return users
- 
 
+def periodic_poll():
+    while 1:
+        poll(timeout=0.05)  # seconds
 
-port = 8888
-server = Listener(port, MyHandler)
-while 1:
-    poll(timeout=0.05) # in seconds
+def start_server():
+    port = 8888
+    Listener(port, ServerHandler)
+    #server = Listener(port, MyHandler)
+    
+    thread = Thread(target=periodic_poll)
+    thread.daemon = True  # die when the main thread dies 
+    thread.start()
